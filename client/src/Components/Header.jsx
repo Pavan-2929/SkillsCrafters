7de48c73725c080ebe10af3be81b84fd7d65e4d7
Pavan-2929 <kapadiyapavan3218@gmail.com>
2024-01-11 22:56:14 +0530

    fixed Header for the better performance

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -5,17 +5,56 @@ import { useAuth } from "../store/Auth";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  
   const { isLoggedIn } = useAuth();
-  const { isLoading, user} = useAuth()
-
-    if (isLoading) {
-      return <h1 className="text-2xl">Please wait...</h1>;
-    }
-
+  const { isLoading, user } = useAuth();
+  
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  if (isLoading) {
+    return (
+      <nav className={`${isMenuOpen ? "bg-[#414141]" : ""} p-4 font-semibold`}>
+        <div className="md:flex justify-around items-center">
+          <div className="text-[2rem] flex justify-around items-center relative">
+            <span className="text-purple-500 animate-fire">SkillsCrafters</span>
+            <div onClick={toggleMenu} className="md:hidden">
+              {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </div>
+          </div>
+
+          <div>
+            <ul
+              className={`text-[1.3rem] md:flex ${
+                isMenuOpen ? "block" : "hidden"
+              } space-y-8 md:space-y-0 items-center flex flex-col md:flex-row justify-center `}
+            >
+              <li className="md:ml-5 xl:mx-5 sm:mt-0 mt-10 hover:text-purple-700">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className="md:ml-5 xl:mx-5 hover:text-purple-700">
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li className="md:ml-5 xl:mx-5 hover:text-purple-700">
+                <NavLink to="/service">Service</NavLink>
+              </li>
+              <li className="md:ml-5 xl:mx-5 hover:text-purple-700">
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <li className="md:ml-5 xl:mx-5 hover:text-purple-700">
+                <NavLink to="/register">Register</NavLink>
+              </li>
+              <li className="md:ml-5 xl:mx-5 hover:text-purple-700">
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
 
   return (
     <nav className={`${isMenuOpen ? "bg-[#414141]" : ""} p-4 font-semibold`}>
